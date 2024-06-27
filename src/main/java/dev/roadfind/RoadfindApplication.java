feb94e79d19f913e995a361d9722374e496bdb70
twonezero <dnjsdudtm98@naver.com>
2024-06-27 17:52:47 +0900

    first commit

---
+++ src/main/java/dev/roadfind/RoadfindApplication.java
@@ -0,0 +1,13 @@
+package dev.roadfind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadfindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadfindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/roadfind/RoadfindApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.roadfind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadfindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
